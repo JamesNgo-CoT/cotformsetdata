4bc210dfc29a886faca9be39768b32f2e08c61fd
James Ngo <James.Ngo@toronto.ca>
2017-05-05 10:55:45 -0400

    Changed "data" to "val", Add daterangepicker Update

--- cotformsetdata.js
+++ cotformsetdata.js
@@ -1,27 +1,26 @@
 CotForm.prototype.setData = function(data) {
 	// STANDARD FIELD OPERATION
-	function standardFieldOp(field, data) {
+	function standardFieldOp(field, val) {
 		if (field.length === 1) { // SINGLE FIELD ELMENT
-			if (Array.isArray(data)) { // MULTIPLE VALUE ELEMENT - AKA SELECT
-				for (var i = 0, l = data.length; i < l; i++) {
-					field.find('[value="' + data[i] + '"]').prop('selected', true);
+			if (Array.isArray(val)) { // MULTIPLE VALUE ELEMENT - AKA SELECT
+				for (var i = 0, l = val.length; i < l; i++) {
+					field.find('[value="' + val[i] + '"]').prop('selected', true);
 				}
 			} else { // STANDARD TEXT-LIKE FIELD
-				field.val(data);
+				field.val(val);
 			}
 		} else { // MULTIPLE FIELD ELEMENT - GROUP OF CHECKBOXS, RADIO BUTTONS
-			if (Array.isArray(data)) {
-				for (var i = 0, l = data.length; i < l; i++) {
-					field.filter('[value="' + data[i] + '"]').prop('checked', true);
+			if (Array.isArray(val)) {
+				for (var i = 0, l = val.length; i < l; i++) {
+					field.filter('[value="' + val[i] + '"]').prop('checked', true);
 				}
 			} else { // SINGLE FIELD ELEMENT - STAND ALONE CHECKBOX, RADIO BUTTON
-				field.filter('[value="' + data + '"]').prop('checked', true);
+				field.filter('[value="' + val + '"]').prop('checked', true);
 			}
 		}
 		// PLUGIN REBUILD
-		if (field.is('.multiselect')) {
-			field.multiselect('rebuild');
-		}
+		field.filter('.multiselect').multiselect('rebuild');
+		field.filter('.daterangevalidation').daterangepicker('update');
 	}
 	// GO THROUGH DATA
 	var form = $('#' + this.cotForm.id);
